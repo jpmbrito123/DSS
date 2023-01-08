580ebac6c0b33d66d315f83a110c279e3850ee33
jpmbrito123 <73346748+jpmbrito123@users.noreply.github.com>
2023-01-08 00:12:51 +0000

    Delete Fase 3 directory

--- Fase 3/projeto/src/data/DAOconfig.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-public class DAOconfig {
-    static final String USERNAME = "root";                       // Actualizar
-    static final String PASSWORD = "1234";                       // Actualizar
-    private static final String DATABASE = "new";          // Actualizar
-    private static final String DRIVER = "jdbc:mysql";        // Usar para MySQL
-    static final String URL = DRIVER+"://localhost:3306/"+DATABASE;
-}
